e6ac2762e44933cd036a96dab1d7e191c3646ccb
Yusuke Hakamaya <yhakamay@c1r5s3.42tokyo.jp>
2021-03-02 15:43:37 +0900

    add: raycasting-c/Makefile and raycasting-c/src/

---
+++ raycasting-c/src/constants.h
@@ -0,0 +1,8 @@
+#define FALSE 0
+#define TRUE 1
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define FPS 30
+#define FRAME_TIME_LENGTH (1000 / FPS)
---
+++ raycasting-c/src/main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int isGameRunning = FALSE;
+int ticksLastFrame;
+
+int playerX, playerY;
+
+int initializeWindow() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    }
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return FALSE;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return FALSE;
+    }
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    return TRUE;
+}
+
+void destroyWindow() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void setup() {
+    playerX = 0;
+    playerY = 0;
+}
+
+void processInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT: {
+            isGameRunning = FALSE;
+            break;
+        }
+        case SDL_KEYDOWN: {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                isGameRunning = FALSE;
+            break;
+        }
+    }
+}
+
+void update() {
+    // Compute how long we have until the reach the target frame time in milliseconds
+    int timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
+
+    // Only delay execution if we are running too fast
+    if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
+        SDL_Delay(timeToWait);
+    }
+
+    float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+    // Store the milliseconds of the current frame to be used in the future
+    ticksLastFrame = SDL_GetTicks();
+
+    playerX += 50 * deltaTime;
+    playerY += 50 * deltaTime;
+}
+
+void render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_Rect rect = {playerX, playerY, 20, 20};
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+}
+
+int main() {
+    isGameRunning = initializeWindow();
+
+    setup();
+
+    while (isGameRunning) {
+        processInput();
+        update();
+        render();
+    }
+
+    destroyWindow();
+
+    return 0;
+}
