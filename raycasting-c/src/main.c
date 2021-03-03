3b3eb4c3deaac30e3acafbafb3d2dd90dd8cf54d
yakamay <yhakamay@student.42tokyo.jp>
2021-03-03 15:21:38 +0900

    add: some cases which decide the angle of move

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -142,6 +142,25 @@ void processInput() {
         case SDL_KEYDOWN: {
             if (event.key.keysym.sym == SDLK_ESCAPE)
                 isGameRunning = FALSE;
+            if (event.key.keysym.sym == SDLK_UP)
+                player.walkDirection = +1;
+            if (event.key.keysym.sym == SDLK_DOWN)
+                player.walkDirection = -1;
+            if (event.key.keysym.sym == SDLK_RIGHT)
+                player.turnDirection = +1;
+            if (event.key.keysym.sym == SDLK_LEFT)
+                player.turnDirection = -1;
+            break;
+        }
+        case SDL_KEYUP: {
+            if (event.key.keysym.sym == SDLK_UP)
+                player.walkDirection = 0;
+            if (event.key.keysym.sym == SDLK_DOWN)
+                player.walkDirection = 0;
+            if (event.key.keysym.sym == SDLK_RIGHT)
+                player.turnDirection = 0;
+            if (event.key.keysym.sym == SDLK_LEFT)
+                player.turnDirection = 0;
             break;
         }
     }
