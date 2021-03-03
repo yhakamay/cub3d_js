68960a93edd165be231a7b323c81d9667562f106
yakamay <yhakamay@student.42tokyo.jp>
2021-03-03 15:20:56 +0900

    add: move and render functions
    movePlayer(), renderPlayer(), and renderMap()

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -69,7 +69,66 @@ void destroyWindow() {
 }
 
 void setup() {
-    player.x = 0;
+    player.x = WINDOW_WIDTH / 2;
+	player.y = WINDOW_HEIGHT / 2;
+	player.width = 1;
+	player.height = 1;
+    player.turnDirection = 0;
+    player.walkDirection = 0;
+    player.rotationAngle = PI / 2;
+    player.walkSpeed = 200;
+    player.turnSpeed = 90 * (PI / 180);
+}
+
+void movePlayer(float deltaTime) {
+    player.rotationAngle += player.turnDirection * player.turnSpeed * deltaTime;
+    float moveStep = player.walkDirection * player.walkSpeed * deltaTime;
+
+    float newPlayerX = player.x + cos(player.rotationAngle) * moveStep;
+    float newPlayerY = player.y + sin(player.rotationAngle) * moveStep;
+
+    player.x = newPlayerX;
+    player.y = newPlayerY;
+    // TODO: 
+    // perform wall collision
+}
+
+void renderPlayer() {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_Rect    playerRect = {
+        player.x * MINIMAP_SCALE_FACTOR,
+        player.y * MINIMAP_SCALE_FACTOR,
+        player.width * MINIMAP_SCALE_FACTOR,
+        player.height * MINIMAP_SCALE_FACTOR
+    };
+    SDL_RenderFillRect(renderer, &playerRect);
+
+    SDL_RenderDrawLine(
+        renderer,
+        player.x * MINIMAP_SCALE_FACTOR,
+        player.y * MINIMAP_SCALE_FACTOR,
+        player.x + cos(player.rotationAngle) * 40 * MINIMAP_SCALE_FACTOR,
+        player.y + sin(player.rotationAngle) * 40 * MINIMAP_SCALE_FACTOR
+    );
+}
+
+void renderMap() {
+    for (int i = 0; i < MAP_NUM_ROWS; i++) {
+        for (int j = 0; j < MAP_NUM_COLS; j++) {
+            int tileX = j * TILE_SIZE;
+            int tileY = i * TILE_SIZE;
+            int tileColor = map[i][j] != 0 ? 255 : 0;
+            
+            SDL_SetRenderDrawColor(renderer, tileColor, tileColor, tileColor, 255);
+            SDL_Rect mapTileRect = {
+                tileX * MINIMAP_SCALE_FACTOR,
+                tileY * MINIMAP_SCALE_FACTOR,
+                TILE_SIZE * MINIMAP_SCALE_FACTOR,
+                TILE_SIZE * MINIMAP_SCALE_FACTOR
+            };
+            SDL_RenderFillRect(renderer, &mapTileRect);
+        }   
+    }
 }
 
 void processInput() {
@@ -90,20 +149,13 @@ void processInput() {
 
 void update() {
     // Compute how long we have until the reach the target frame time in milliseconds
-    int timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
-
-    // Only delay execution if we are running too fast
-    if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
-        SDL_Delay(timeToWait);
-    }
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TIME_LENGTH));
 
     float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
 
-    // Store the milliseconds of the current frame to be used in the future
     ticksLastFrame = SDL_GetTicks();
 
-    // playerX += 50 * deltaTime;
-    // playerY += 50 * deltaTime;
+    movePlayer(deltaTime);
 }
 
 void render() {
@@ -112,6 +164,9 @@ void render() {
 
 	// TODO:
 	// Render game objects here
+    renderMap();
+    // renderRays();
+    renderPlayer();
 
     SDL_RenderPresent(renderer);
 }
